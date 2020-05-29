f6dea21565a134584f4c58203aab47e088dacec3
muamba-vrpex <66101564+muamba-vrpex@users.noreply.github.com>
2020-05-29 02:51:44 -0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,7 +5,6 @@ local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP")
-src = Tunnel.getInterface("vrp_diagnostic")
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- DIAGNOSTIC
 -----------------------------------------------------------------------------------------------------------------------------------------
